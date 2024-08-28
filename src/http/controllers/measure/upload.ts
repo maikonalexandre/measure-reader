2bff33729ef42db0fbaf388bea04e82d726f60fd
Maike <maikonalexandre574@gmail.com>
2024-08-28 20:41:26 -0300

    feat: create upload route

--- src/http/controllers/measure/upload.ts
+++ src/http/controllers/measure/upload.ts
@@ -1,5 +1,68 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { env } from '../../../env';
+
+import { writeFile, writeFileSync } from 'node:fs';
+
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleAIFileManager } from '@google/generative-ai/server';
+import { fileSystemImageStorageRepository } from '../../../repositories/fileSystemStorage/file-system-image-storage-repository';
+import { SaveImageUseCase } from '../../../use-cases/save-image-use-case';
+import { getImageTypeFromBase64 } from '../../../utils';
+
+const uploadImageSchema = z.object({
+	image: z
+		.string()
+		.base64()
+		.refine((value) => {
+			const imgType = getImageTypeFromBase64(value);
+			if (imgType === 'unknown') {
+				return false;
+			}
+		}),
+	customer_code: z.string(),
+	measure_datetime: z.coerce.date(),
+	measure_type: z.enum(['WATER', 'GAS']),
+});
 
 export async function upload(request: FastifyRequest, reply: FastifyReply) {
+	const { customer_code, image, measure_datetime, measure_type } =
+		uploadImageSchema.parse(request.body);
+
+	const saveImageUseCase = new SaveImageUseCase(
+		fileSystemImageStorageRepository,
+	);
+
+	const { imagePath, imagePreviewURL } = await saveImageUseCase.execute({
+		image,
+	});
+
+	console.log(imagePath, imagePreviewURL);
+
+	const fileManager = new GoogleAIFileManager(env.GEMINI_API_KEY);
+
+	const uploadResponse = await fileManager.uploadFile(imagePath, {
+		mimeType: 'image/jpeg',
+	});
+
+	const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
+	const model = genAI.getGenerativeModel({
+		model: 'gemini-1.5-flash',
+	});
+
+	const result = await model.generateContent([
+		{
+			fileData: {
+				mimeType: uploadResponse.file.mimeType,
+				fileUri: uploadResponse.file.uri,
+			},
+		},
+		{
+			text: 'This is a picture of a measurement record. I need the exact number that is being shown in the record, just the number, nothing more.',
+		},
+	]);
+
+	console.log('RESULT', result.response.text());
+
 	reply.code(200).send({ message: 'OK' });
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { app } from './app';
 import { env } from './env';
 
 app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-	console.log('✅ Server is running');
+	console.log('🚀 Server is running');
 });

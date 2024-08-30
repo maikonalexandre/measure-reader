a9772ebb18446d7a710956f57d1aca5d0923c803
Maike <maikonalexandre574@gmail.com>
2024-08-30 18:42:00 -0300

    chore: change server baseurl

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
 	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
 	PORT: z.coerce.number().default(3333),
-	SERVER_BASE_URL: z.string().url().default('http://localhost:3333'),
+	SERVER_BASE_URL: z.string().url().default('http://localhost:80'),
 	GEMINI_API_KEY: z.string(),
 	HOST: z.string().default('0.0.0.0'),
 	GEMINI_MAX_RETRIES: z.number().default(1),

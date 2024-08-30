61ccbf848e234cdf473a0a6f43448b8977751f4c
Maike <maikonalexandre574@gmail.com>
2024-08-30 09:15:37 -0300

    refactor: remove prisma log

--- src/http/controllers/measure/upload.ts
+++ src/http/controllers/measure/upload.ts
@@ -1,13 +1,11 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
-import { env } from '../../../env';
 
-import { writeFile, writeFileSync } from 'node:fs';
-
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleAIFileManager } from '@google/generative-ai/server';
 import { fileSystemImageStorageRepository } from '../../../repositories/fileSystemStorage/file-system-image-storage-repository';
-import { SaveImageUseCase } from '../../../use-cases/save-image-use-case';
+import { geminiImageScanRepository } from '../../../repositories/gemini/gemini-image-scan-repository';
+import { prismaMeasureRepository } from '../../../repositories/prisma/prisma-measure-repository';
+import { CreateMeasureUseCase } from '../../../use-cases/create-measure';
+import { MeasureAlreadyExistsError } from '../../../use-cases/errors/measure-already-exists';
 import { getImageTypeFromBase64 } from '../../../utils';
 
 const uploadImageSchema = z.object({
@@ -15,10 +13,11 @@ const uploadImageSchema = z.object({
 		.string()
 		.base64()
 		.refine((value) => {
-			const imgType = getImageTypeFromBase64(value);
-			if (imgType === 'unknown') {
+			if (getImageTypeFromBase64(value) === 'unknown') {
 				return false;
 			}
+
+			return true;
 		}),
 	customer_code: z.string(),
 	measure_datetime: z.coerce.date(),
@@ -29,40 +28,30 @@ export async function upload(request: FastifyRequest, reply: FastifyReply) {
 	const { customer_code, image, measure_datetime, measure_type } =
 		uploadImageSchema.parse(request.body);
 
-	const saveImageUseCase = new SaveImageUseCase(
-		fileSystemImageStorageRepository,
-	);
+	try {
+		const createMeasureUseCase = new CreateMeasureUseCase(
+			fileSystemImageStorageRepository,
+			prismaMeasureRepository,
+			geminiImageScanRepository,
+		);
 
-	const { imagePath, imagePreviewURL } = await saveImageUseCase.execute({
-		image,
-	});
+		const measure = await createMeasureUseCase.execute({
+			customer_code,
+			measure_datetime,
+			measure_type,
+			imageBase64: image,
+		});
 
-	console.log(imagePath, imagePreviewURL);
+		//TODO: corrigir retorno
+		reply.code(200).send({ message: 'OK', ...measure });
+	} catch (error) {
+		if (error instanceof MeasureAlreadyExistsError) {
+			//TODO: corrigir retorno
+			return reply.status(409).send({ message: error.message });
+		}
 
-	const fileManager = new GoogleAIFileManager(env.GEMINI_API_KEY);
+		console.log(error);
+	}
 
-	const uploadResponse = await fileManager.uploadFile(imagePath, {
-		mimeType: 'image/jpeg',
-	});
-
-	const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
-	const model = genAI.getGenerativeModel({
-		model: 'gemini-1.5-flash',
-	});
-
-	const result = await model.generateContent([
-		{
-			fileData: {
-				mimeType: uploadResponse.file.mimeType,
-				fileUri: uploadResponse.file.uri,
-			},
-		},
-		{
-			text: 'This is a picture of a measurement record. I need the exact number that is being shown in the record, just the number, nothing more.',
-		},
-	]);
-
-	console.log('RESULT', result.response.text());
-
-	reply.code(200).send({ message: 'OK' });
+	reply.code(200).send({ message: 'oa' });
 }

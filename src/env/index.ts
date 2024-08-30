a4165d21666cee2403921d2d04af35a750b5aebf
Maike <maikonalexandre574@gmail.com>
2024-08-30 09:10:52 -0300

    feat: add some properties validation on env

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,8 +3,10 @@ import { z } from 'zod';
 const envSchema = z.object({
 	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
 	PORT: z.coerce.number().default(3333),
+	SERVER_BASE_URL: z.string().url().default('http://localhost:3333'),
 	GEMINI_API_KEY: z.string(),
 	HOST: z.string().default('0.0.0.0'),
+	GEMINI_MAX_RETRIES: z.number().default(1),
 });
 
 const _env = envSchema.safeParse(process.env);

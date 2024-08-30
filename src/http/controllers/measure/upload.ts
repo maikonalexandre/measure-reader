304cc2326100f75cd721f90bd98433fd0e2c4975
Maike <maikonalexandre574@gmail.com>
2024-08-30 10:41:43 -0300

    refactor: refact upload reply

--- src/http/controllers/measure/upload.ts
+++ src/http/controllers/measure/upload.ts
@@ -1,11 +1,12 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
+import { env } from '../../../env';
 import { fileSystemImageStorageRepository } from '../../../repositories/fileSystemStorage/file-system-image-storage-repository';
 import { geminiImageScanRepository } from '../../../repositories/gemini/gemini-image-scan-repository';
 import { prismaMeasureRepository } from '../../../repositories/prisma/prisma-measure-repository';
 import { CreateMeasureUseCase } from '../../../use-cases/create-measure';
-import { MeasureAlreadyExistsError } from '../../../use-cases/errors/measure-already-exists';
+import { MeasureAlreadyExistsError } from '../../../use-cases/errors/measure-already-exists-error';
 import { getImageTypeFromBase64 } from '../../../utils';
 
 const uploadImageSchema = z.object({
@@ -42,16 +43,31 @@ export async function upload(request: FastifyRequest, reply: FastifyReply) {
 			imageBase64: image,
 		});
 
-		//TODO: corrigir retorno
-		reply.code(200).send({ message: 'OK', ...measure });
+		reply.code(200).send({
+			message: 'Operação realizada com sucesso',
+			status: {
+				code: 200,
+				description: 'Operação realizada com sucesso',
+			},
+			image_url: `${env.SERVER_BASE_URL}/${measure.image_address}`,
+			measure_value: measure.measure_value,
+			measure_uuid: measure.id,
+		});
 	} catch (error) {
 		if (error instanceof MeasureAlreadyExistsError) {
-			//TODO: corrigir retorno
-			return reply.status(409).send({ message: error.message });
+			return reply.status(409).send({
+				message: 'Já existe uma leitura para este tipo no mês atual',
+				status: {
+					code: 409,
+					description: 'Já existe uma leitura para este tipo no mês atual',
+				},
+				error_code: 'DOUBLE_REPORT',
+				error_description: 'Leitura do mês já realizada',
+			});
 		}
 
-		console.log(error);
-	}
+		//TODO: tratar demais erros, imagem, gemini etc...
 
-	reply.code(200).send({ message: 'oa' });
+		throw error;
+	}
 }

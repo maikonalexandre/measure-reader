632d9e1d80be46ddc54284b5dfc0d7f0be1bf2e6
Maike <maikonalexandre574@gmail.com>
2024-08-30 18:38:38 -0300

    feat: add static sever

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import { ZodError } from 'zod';
 import { appRoutes } from './http/routes';
 
+import { join } from 'node:path';
 import cors from '@fastify/cors';
 
 export const app = Fastify();
@@ -11,16 +12,19 @@ app.register(cors, {
 	methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
 });
 
+console.log(__dirname, 'uploads');
+
+app.register(require('@fastify/static'), {
+	root: join(__dirname, '..', 'uploads'),
+	prefix: '/uploads/',
+});
+
 app.register(appRoutes);
 
 app.setErrorHandler((error, request, reply) => {
 	if (error instanceof ZodError) {
 		return reply.status(400).send({
 			message: 'Os dados fornecidos no corpo da requisição são inválidos',
-			status: {
-				code: 400,
-				description: 'Os dados fornecidos no corpo da requisição são inválidos',
-			},
 			error_code: 'INVALID_DATA',
 			error_description: error.format(),
 		});
